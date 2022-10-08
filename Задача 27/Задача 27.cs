0969c7f804b7f6cef6dfe6529cee9c63590abaa2
lightlana <lebendaufdensternen@gmail.com>
2022-10-08 16:46:54 +0300

    программа, которая принимает на вход число и выдаёт сумму цифр в числе.

---
+++ Задача 27/Задача 27.cs
@@ -0,0 +1,23 @@
+﻿void task27()
+{
+    Console.WriteLine("Введите число: "); 
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number < 10)
+    {
+        Console.Write($"{number}");
+    }
+
+    else
+    {
+        int sum = 0;
+        while (number > 0) 
+        {
+            sum = sum + number % 10;
+            number = number / 10;
+        }
+         Console.Write($"{sum}");
+    }
+
+}
+
+task27();
