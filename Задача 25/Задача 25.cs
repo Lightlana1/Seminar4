11dfb8610a44ff798748298319e02f8b8edf8289
lightlana <lebendaufdensternen@gmail.com>
2022-10-08 16:39:09 +0300

    цикл, который принимает на вход два натуральных числа (A и B) и возводит число A в степень B

---
+++ Задача 25/Задача 25.cs
@@ -0,0 +1,17 @@
+﻿void task25()
+{
+    Console.WriteLine("Введите число: "); 
+    
+    int number1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите степень: "); 
+    int count = Convert.ToInt32(Console.ReadLine());
+    int result = 1;
+
+    for (int i =1; i <= count; i++)
+    {
+        result *= number1;
+    }  
+
+    Console.Write($"{result}");
+}
+task25();
