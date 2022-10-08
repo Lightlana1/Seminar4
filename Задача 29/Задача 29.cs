8334962dff550139d1cf72dc9279c8ec11125b93
lightlana <lebendaufdensternen@gmail.com>
2022-10-08 16:48:43 +0300

    программа, которая задаёт массив из 8 случайных целых чисел и выводит отсортированный по модулю массив.

---
+++ Задача 29/Задача 29.cs
@@ -0,0 +1,43 @@
+﻿void task29()
+{
+    int size = 8;
+    int[] numbers = new int[size];
+    FillArray(numbers);
+    PrintArray(numbers);
+
+    for (int i = 1; i < size; i++)
+    {
+        for (int j = 0; j < size - i; j++)
+        {
+            if (Math.Abs(numbers[j]) > Math.Abs(numbers[j+1]))
+            {
+                int temp = numbers[j];
+                numbers[j] = numbers[j+1];
+                numbers[j+1]=temp;
+            }
+        }
+    }
+     PrintArray(numbers);
+
+}
+
+void FillArray(int[] num)
+{
+    for (int i = 0; i < num.Length; i++)
+    {
+        Random random = new Random();
+        num[i] = random.Next(-8,8);
+    }
+
+}
+
+void PrintArray(int[] num)
+{
+    for (int i = 0; i < num.Length; i++)
+    {
+        Console.Write(num[i] + "\t");
+    }
+    Console.WriteLine();
+}
+
+ task29();
